37fb550d8fe1933c370cb0abede1af1b4c5c887d
hasithauom <88756920+hasithauom@users.noreply.github.com>
2023-12-11 23:37:44 +0530

    updated_array_block

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import "./App.css";
 import Main from "./Components/Main";
 import { MyData } from "./Data/MyData";
@@ -5,24 +6,29 @@ import { MyData2 } from "./Data/MyData";
 
 const DataBlock = () => {
   return (
-    <>
-      {MyData2.map((ele ,index) => {
+    <Fragment>
+      {MyData2.map((ele, index) => {
         return <Main key={index} name={ele.name} city={ele.city} />;
       })}
-    </>
+    </Fragment>
   );
 };
 
 function App() {
+  const Datablock2 = MyData2.map(({ name, city }, index) => {
+    return <Main key={index} name={name} city={city} />;
+  });
+
   return (
-    <>
+    <Fragment>
       <div>
         {MyData.map(({ name, city }, index) => {
           return <Main key={index} name={name} city={city} />;
         })}
       </div>
       <DataBlock />
-    </>
+      {Datablock2}
+    </Fragment>
   );
 }
 
